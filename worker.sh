e041d93c366e70c998734a313cdcd088c19fb6e5
Wilton Ribeiro <jiquitaiasofware@gmail.com>
2022-11-02 15:00:54 -0300

    docker-swarm cluster

--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-3pj8k0i4tn77bd93a0yxhgh36hxuef5q5oyg1732rztnfy29ll-a94q0ipwgrjs4xikzyb4yb3n5 10.10.10.100:2377
+    docker swarm join --token SWMTKN-1-4v4fyzwnc870hoi8akcytqw9a68ddf7u0nbw2snty1snelbhzp-dilcd57syytqx0bu00ddlocb8 10.10.10.100:2377
